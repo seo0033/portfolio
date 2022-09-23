fa0dd20184ba72f199ded682fb910eeab8e5e020
seo0033 <tkrhk9511@gmail.com>
2022-09-23 17:50:21 +0900

    20220923

--- 05-1_louispoulse/js/main.js
+++ 05-1_louispoulse/js/main.js
@@ -1,10 +1,28 @@
 $(function () {
 
-    $('.show .itm_slide').slick({
+    $('.col .content .itm_slide').slick({
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 1,
-        arrows:false,
+        arrows: false,
     })
+
+    $('.col .t_a a:nth-child(1)').on('click', function (e) {
+        e.preventDefault();
+        $('.col .top .content .itm_slide').slick('slickPrev');
+    });
+    $('.col .t_a a:nth-child(2)').on('click', function (e) {
+        e.preventDefault();
+        $('.col .top .content .itm_slide').slick('slickNext');
+    });
+
+    $('.col .b_a a:nth-child(1)').on('click', function (e) {
+        e.preventDefault();
+        $('.col .bt .content .itm_slide').slick('slickPrev');
+    });
+    $('.col .b_a a:nth-child(2)').on('click', function (e) {
+        e.preventDefault();
+        $('.col .bt .content .itm_slide').slick('slickNext');
+    });
 
 })//the end
---
+++ 11_DLE&C/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="Wrap">
+      
+    </div>
+  );
+}
+
+export default App;
