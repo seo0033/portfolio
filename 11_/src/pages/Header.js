aa38a3bd87939643f709e0d851eaa1f773a5b832
seo0033 <tkrhk9511@gmail.com>
2022-09-27 17:50:50 +0900

    20220927

--- 11_/src/pages/Header.js
+++ 11_/src/pages/Header.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='Header'>
+      <h1>
+        <Link to='/'>
+          <img src={process.env.PUBLIC_URL + "/assets/logo.png"} alt="" />
+        </Link>
+      </h1>
+    </header>
   )
 }
 
