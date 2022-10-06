5cf92a42a799f07aa6c79489de237e517821d745
seo0033 <tkrhk9511@gmail.com>
2022-10-07 00:29:50 +0900

    20221007

--- 11_doosi/src/pages/Marcket_P03.js
+++ 11_doosi/src/pages/Marcket_P03.js
@@ -5,4 +5,23 @@ import M_Plant from '../components/M_Plant';
 import '../css/Spage.scss'
 
 
-const Sub01_0;
+const Sub01_02 = ({ Content }) => {
+    const num = 0;
+    return (
+        <section className='marcket sub01_02'>
+            <div className="sub_v sc">
+                <figure>
+                    <img src={process.env.PUBLIC_URL + "/assets/img/sub_bg_01.png"} alt="" />
+                    <h2>{Content[num].pha}</h2>
+                </figure>
+            </div>
+            <div className="inner sub_sc">
+                <div className='sub_cate'>{Content[num].con} / {CATEGORY[0].tit}</div>
+                <div className="sub_tit b_bottom">{CATEGORY[0].tit}</div>
+                <M_Plant />
+            </div>
+        </section>
+    )
+}
+
+export default Sub01_02
